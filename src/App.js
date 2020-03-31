3af96c13d52dcdb89c4602bd4aaf6e6843448e15
Willjunger <williamjunger@hotmail.com>
2020-03-31 15:32:20 -0300

    biscoito da sorte

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./estilo.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			textoFrase: ""
+		};
+		this.quebraBiscoito = this.quebraBiscoito.bind(this);
+		this.frases = ["A vida trará coisas boas se tiveres paciência.", "Defeitos e virtudes são apenas dois lados da mesma moeda.", "A maior de todas as torres começa no solo.", "Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.", "Siga os bons e aprenda com eles.", "Não importa o tamanho da montanha, ela não pode tapar o sol."];
+	}
+
+	quebraBiscoito() {
+		let state = this.state;
+		let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+		state.textoFrase = '"' + this.frases[numeroAleatorio] + '"';
+		this.setState(state);
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<img src={require("./assets/biscoito.png")} alt="biscoito da sorte" className="img" />
+				<Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito} />
+				<h3 className="textoFrase">{this.state.textoFrase}</h3>
+			</div>
+		);
+	}
+}
+
+class Botao extends Component {
+	render() {
+		return (
+			<div>
+				<button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+			</div>
+		);
+	}
 }
 
 export default App;
